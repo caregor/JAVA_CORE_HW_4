97d55d2564a5cfdd148417c0d3d12cbd04657253
caregor <64469336+caregor@users.noreply.github.com>
2024-01-25 21:55:53 +0500

    Task 1 is done. Added emun Gender; added setters for Customer;

--- src/main/java/ru/gb/hw/Customer.java
+++ src/main/java/ru/gb/hw/Customer.java
@@ -6,11 +6,13 @@ public class Customer {
     private String FIO;
     private LocalDate dateOfBirth;
     private String phone;
+    private Gender gender;
 
-    public Customer(String FIO, LocalDate dateOfBirth, String phone) {
+    public Customer(String FIO, LocalDate dateOfBirth, String phone, Gender gender) {
         this.FIO = FIO;
         this.dateOfBirth = dateOfBirth;
         this.phone = phone;
+        this.gender = gender;
     }
 
     public String getFIO() {
@@ -23,5 +25,21 @@ public class Customer {
 
     public String getPhone() {
         return phone;
+    }
+
+    public void setFIO(String FIO) {
+        this.FIO = FIO;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
     }
 }
